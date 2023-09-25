6666341063a1b6cae5d7289d1ad996882c2a49dd
5656831 <hasegawatatsuya206@gmail.com>
2023-09-25 15:05:19 +0900

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === "production";
+const withPWA = require("next-pwa")({
+  disable: isProd ? false : true,
+  dest: "public",
+});
 
-module.exports = nextConfig
+module.exports = withPWA();
