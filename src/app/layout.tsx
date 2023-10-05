b5f2fd01707e97f25a41253a2aeb7bd283caf876
5656831 <hasegawatatsuya206@gmail.com>
2023-10-05 20:07:09 +0900

    Feat: disable pull to refresh iOS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className="overflow-hidden overscroll-none">
       <body className={notoSansJP.className}>{children}</body>
     </html>
   );
