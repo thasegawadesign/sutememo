9fe9c9181bfcfe4b87bc6b8ac9d32370864d1edb
5656831 <hasegawatatsuya206@gmail.com>
2023-10-06 04:32:37 +0900

    Add: role属性など

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,21 +14,25 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icons/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/icons/icon-256x256.png',
         sizes: '256x256',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/icons/icon-384x384.png',
         sizes: '384x384',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/icons/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   };
