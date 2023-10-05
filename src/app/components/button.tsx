f278c88a15dc7b8ccd1e4080e0d82f8f0533b3a5
5656831 <hasegawatatsuya206@gmail.com>
2023-10-06 00:49:25 +0900

    Fix: iOSのセーフエリア bottom

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react';
+import { isIOS } from 'react-device-detect';
 import { IoIosAdd } from 'react-icons/io';
 
 type Props = {
@@ -10,7 +11,9 @@ export default forwardRef(function Button(props: Props) {
   return (
     <button
       onClick={handleBtnClick}
-      className="text-white bg-main rounded-lg p-2 text-5xl w-[calc(100%-40px)] transition filter hover:brightness-95 fixed bottom-3 left-0 right-0 mx-auto"
+      className={`text-white bg-main rounded-lg p-2 text-5xl w-[calc(100%-40px)] transition filter hover:brightness-95 fixed bottom-5 left-0 right-0 mx-auto ${
+        isIOS && 'bottom-[calc(0.6vh+20px)]'
+      }`}
     >
       <div className="grid place-items-center">
         <IoIosAdd />
