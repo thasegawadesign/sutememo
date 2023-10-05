99de41277dfdd5d623d893e285e4b590fc25156e
5656831 <hasegawatatsuya206@gmail.com>
2023-10-06 05:33:32 +0900

    Fix: ReferenceError 修正

--- src/app/components/Analytics.tsx
+++ src/app/components/Analytics.tsx
@@ -17,13 +17,13 @@ export default function Analytics() {
       ></Script>
       <Script
         id="google-analytics"
-        strategy={'afterInteractive'}
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', ${ANALYTICS_ID});
+            gtag('config', '${ANALYTICS_ID}');
           `,
         }}
       />
