207933066b4b64e63653524505ffc2845af0fd12
5656831 <hasegawatatsuya206@gmail.com>
2023-10-06 04:40:41 +0900

    Fix: icons purpose setting

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,19 +20,19 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icons/icon-256x256.png',
         sizes: '256x256',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
       {
         src: '/icons/icon-384x384.png',
         sizes: '384x384',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
       {
         src: '/icons/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'maskable',
+        purpose: 'any',
       },
     ],
   };
