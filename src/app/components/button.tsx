fa6f4b65e4b57ad641d3bb49be8d683db962ce0c
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-10-10 13:12:07 +0900

    Fix: モバイルのときだけセーフエリア(bottom)

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react';
+import { isMobile } from 'react-device-detect';
 import { IoIosAdd } from 'react-icons/io';
 
 type Props = {
@@ -13,7 +14,9 @@ export default forwardRef(function Button(props: Props, _ref) {
       aria-label="Add"
       role="button"
       tabIndex={0}
-      className="fixed bottom-5 left-0 right-0 mx-auto w-[calc(100%-40px)] rounded-lg bg-main p-2 text-5xl text-white filter transition hover:brightness-95 pwa:bottom-[env(safe-area-inset-bottom)]"
+      className={`fixed bottom-5 left-0 right-0 mx-auto w-[calc(100%-40px)] rounded-lg bg-main p-2 text-5xl text-white filter transition hover:brightness-95 ${
+        isMobile && 'pwa:bottom-[env(safe-area-inset-bottom)]'
+      }`}
     >
       <div className="grid place-items-center">
         <IoIosAdd />
