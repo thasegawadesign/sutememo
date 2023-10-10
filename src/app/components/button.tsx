dee301326a683671b739995640133845eb2c8df3
5656831 <hasegawatatsuya206@gmail.com>
2023-10-11 06:26:49 +0900

    Add: Google Analytics イベント(add, delete, sort) 設定

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -13,7 +13,7 @@ export default forwardRef(function Button(props: Props, _ref) {
       aria-label="Add"
       role="button"
       tabIndex={0}
-      className="fixed bottom-5 left-0 right-0 mx-auto w-[calc(100%-40px)] rounded-lg bg-main p-2 text-5xl
+      className="add-button fixed bottom-5 left-0 right-0 mx-auto w-[calc(100%-40px)] rounded-lg bg-main p-2 text-5xl
       text-white filter transition hover:brightness-95 pwa:bottom-[max(env(safe-area-inset-bottom),20px)]"
     >
       <div className="grid place-items-center">
