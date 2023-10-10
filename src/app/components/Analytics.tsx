ce87f5fae5f95167d90412df6a30fbcc96d50b6f
5656831 <hasegawatatsuya206@gmail.com>
2023-10-11 06:26:49 +0900

    Add: Google Analytics イベント(add, delete, sort) 設定

--- src/app/components/Analytics.tsx
+++ src/app/components/Analytics.tsx
@@ -9,6 +9,60 @@ export default function Analytics() {
     console.warn('Analytics測定IDが定義されていません');
     return <></>;
   }
+  // const deleteButtons = document.querySelectorAll('.delete-button');
+  // if (deleteButtons.length > 0) {
+  //   deleteButtons.forEach((deleteButton) => {
+  //     deleteButton.addEventListener('click', (event) => {
+  //       gtag('event', 'delete_todo');
+  //     });
+  //   });
+  // }
+
+  // document.addEventListener('mouseup', (event) => {
+  //   if (!event.target) return;
+  //   const isSVG = event.target.localName === 'svg';
+  //   const isButton = event.target.localName === 'button';
+  //   console.log(event);
+  // });
+
+  // document.addEventListener('click', (event) => {
+  //   if (!event.target) return;
+  //   const isSVG = event.target.localName === 'svg';
+  //   const isButton = event.target.localName === 'button';
+  //   const target =
+  //     (isSVG && event.target.parentElement) || (isButton && event.target);
+  //   const isDeleteButton = target.ariaLabel === 'Delete';
+  //   if (isDeleteButton) gtag('event', 'delete_todo');
+  // });
+
+  // document.addEventListener('mouseup', (event) => {
+  //   if (!event.target) return;
+  //   const isSVG = event.target.localName === 'svg';
+  //   const isButton = event.target.localName === 'button';
+  //   const target =
+  //     (isSVG && event.target.parentElement) || (isButton && event.target);
+  //   const isDraggableButton = target.ariaRoleDescription === 'sortable';
+  //   if (isDraggableButton)
+  //     gtag('event', 'sort_todo', {
+  //       event_category: 'Sort',
+  //       event_label: 'MouseUp',
+  //     });
+  // });
+
+  // document.addEventListener('touchend', (event) => {
+  //   if (!event.target) return;
+  //   const isSVG = event.target.localName === 'svg';
+  //   const isButton = event.target.localName === 'button';
+  //   const target =
+  //     (isSVG && event.target.parentElement) || (isButton && event.target);
+  //   const isDraggableButton = target.ariaRoleDescription === 'sortable';
+  //   if (isDraggableButton)
+  //     gtag('event', 'sort_todo', {
+  //       event_category: 'Sort',
+  //       event_label: 'TouchEnd',
+  //     });
+  // });
+
   return (
     <>
       <Script
@@ -24,6 +78,44 @@ export default function Analytics() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${ANALYTICS_ID}');
+            document.querySelector('.add-button')?.addEventListener('click', (event) => {
+              gtag('event', 'add_todo');
+            });
+            document.addEventListener('click', (event) => {
+              if (!event.target) return;
+              const isSVG = event.target.localName === 'svg';
+              const isButton = event.target.localName === 'button';
+              const target =
+                (isSVG && event.target.parentElement) || (isButton && event.target);
+              const isDeleteButton = target.ariaLabel === 'Delete';
+              if (isDeleteButton) gtag('event', 'delete_todo');
+            });
+            document.addEventListener('mouseup', (event) => {
+              if (!event.target) return;
+              const isSVG = event.target.localName === 'svg';
+              const isButton = event.target.localName === 'button';
+              const target =
+                (isSVG && event.target.parentElement) || (isButton && event.target);
+              const isDraggableButton = target.ariaRoleDescription === 'sortable';
+              if (isDraggableButton)
+                gtag('event', 'sort_todo', {
+                  event_category: 'Sort',
+                  event_label: 'MouseUp',
+                });
+            });
+            document.addEventListener('touchend', (event) => {
+              if (!event.target) return;
+              const isSVG = event.target.localName === 'svg';
+              const isButton = event.target.localName === 'button';
+              const target =
+                (isSVG && event.target.parentElement) || (isButton && event.target);
+              const isDraggableButton = target.ariaRoleDescription === 'sortable';
+              if (isDraggableButton)
+                gtag('event', 'sort_todo', {
+                  event_category: 'Sort',
+                  event_label: 'TouchEnd',
+                });
+            });
             window.addEventListener('appinstalled', (event) => {
               gtag('event', 'install_pwa', {
                 event_category: 'PWA',
