4aa44bf08bd946d1c82381221cf52a5a73ba34e0
5656831 <hasegawatatsuya206@gmail.com>
2023-10-16 02:15:19 +0900

    Style: Undo / Redo, ボタンスタイル調整

--- src/app/components/Redo.tsx
+++ src/app/components/Redo.tsx
@@ -1,15 +1,21 @@
 import { ImRedo } from 'react-icons/im';
 
 type Props = {
+  canRedo: boolean;
   handleRedoClick: () => void;
 };
 
 export default function Redo(props: Props) {
-  const { handleRedoClick } = props;
+  const { canRedo, handleRedoClick } = props;
   return (
     <>
       <button
-        className="fixed bottom-24 right-[22px] rounded-full bg-gray-50 p-9 text-2xl text-gray-500 filter transition hover:brightness-95"
+        tabIndex={0}
+        aria-label="Redo"
+        role="button"
+        className={`fixed bottom-[max(calc(env(safe-area-inset-bottom)+92px),102px)] right-[22px] rounded-full border border-gray-200 bg-gray-100 p-8 text-2xl text-gray-500 filter transition hover:brightness-95 ${
+          canRedo ? 'brightness-100' : 'brightness-105 hover:brightness-105'
+        }`}
         onClick={handleRedoClick}
       >
         <ImRedo />
