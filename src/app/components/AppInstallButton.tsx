5554c5669cee0ab2fdf2cf7fbb0c383c1abb28c2
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-10-16 19:56:32 +0900

    Style: Button shadow-none

--- src/app/components/AppInstallButton.tsx
+++ src/app/components/AppInstallButton.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent, RefObject } from 'react';
+import { Button } from '@material-tailwind/react';
 
 type Props = {
   handleAppInstallButtonClick: (event: MouseEvent) => void;
@@ -9,13 +10,13 @@ export default function AppInstallButton(props: Props) {
   const { handleAppInstallButtonClick, appInstallButtonRef } = props;
   return (
     <>
-      <button
+      <Button
         ref={appInstallButtonRef}
         onClick={handleAppInstallButtonClick}
-        className="flex items-center gap-0.5 rounded-full bg-main px-6 py-1.5 font-semibold text-white filter transition hover:brightness-95"
+        className="flex items-center gap-0.5 rounded-full bg-main px-6 py-1.5 font-semibold text-white !shadow-none filter transition hover:brightness-95"
       >
         <span className="select-none text-sm">入手</span>
-      </button>
+      </Button>
     </>
   );
 }
