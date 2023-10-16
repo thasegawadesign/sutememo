2986880b4e30d674e4e24057b43f2c02744a21bf
5656831 <hasegawatatsuya206@gmail.com>
2023-10-16 19:56:32 +0900

    Style: Button shadow-none

--- src/app/components/AddButton.tsx
+++ src/app/components/AddButton.tsx
@@ -18,7 +18,7 @@ export default forwardRef(function AddButton(props: Props, _ref) {
         role="button"
         tabIndex={0}
         className="!fixed bottom-5 left-0 right-0 mx-auto w-[calc(100%-40px)] rounded-lg bg-main p-2 text-5xl
-        text-white filter transition hover:brightness-95 pwa:bottom-[max(env(safe-area-inset-bottom),20px)]"
+        text-white !shadow-none filter transition hover:brightness-95 pwa:bottom-[max(env(safe-area-inset-bottom),20px)]"
       >
         <div className="grid place-items-center">
           <IoIosAdd />
