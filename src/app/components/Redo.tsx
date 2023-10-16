bf34c8ece706cd22236bac1d1da969c69909c517
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-10-16 19:22:42 +0900

    Style: ボタンスタイル ripple 調整

--- src/app/components/Redo.tsx
+++ src/app/components/Redo.tsx
@@ -1,5 +1,5 @@
-import { TouchEvent } from 'react';
 import { ImRedo } from 'react-icons/im';
+import { Button } from '@material-tailwind/react';
 
 type Props = {
   canRedo: boolean;
@@ -11,20 +11,20 @@ export default function Redo(props: Props) {
 
   return (
     <>
-      <button
+      <Button
         tabIndex={0}
-        aria-label="Redo"
+        aria-label="Undo"
         role="button"
-        className={`xs:text-xl xs:p-7 fixed bottom-[max(calc(env(safe-area-inset-bottom)+80px),102px)] right-[22px] rounded-full border border-gray-200 bg-gray-100 p-[26px] text-lg
-        brightness-105 filter transition ${
-          canRedo
-            ? 'text-gray-500 hover:brightness-[102%]'
-            : 'text-gray-300 hover:brightness-105'
+        color="white"
+        variant="text"
+        className={`xs:text-xl xs:p-7 !fixed bottom-[max(calc(env(safe-area-inset-bottom)+72px),94px)] right-[22px] rounded-full p-[26px] text-lg ${
+          canRedo ? 'text-gray-500' : 'text-gray-300'
         }`}
+        ripple={true}
         onClick={handleRedoClick}
       >
         <ImRedo />
-      </button>
+      </Button>
     </>
   );
 }
--- src/app/components/Undo.tsx
+++ src/app/components/Undo.tsx
@@ -1,5 +1,5 @@
-import { TouchEvent } from 'react';
 import { ImUndo } from 'react-icons/im';
+import { Button } from '@material-tailwind/react';
 
 type Props = {
   canUndo: boolean;
@@ -10,19 +10,20 @@ export default function Undo(props: Props) {
   const { canUndo, handleUndoClick } = props;
   return (
     <>
-      <button
+      <Button
         tabIndex={0}
         aria-label="Undo"
         role="button"
-        className={`xs:text-xl xs:p-7 fixed bottom-[max(calc(env(safe-area-inset-bottom)+80px),102px)] left-[22px] rounded-full border border-gray-200 bg-gray-100 p-[26px] text-lg brightness-105 filter transition ${
-          canUndo
-            ? 'text-gray-500 hover:brightness-[102%]'
-            : 'text-gray-300 hover:brightness-105'
+        color="white"
+        variant="text"
+        className={`xs:text-xl xs:p-7 !fixed bottom-[max(calc(env(safe-area-inset-bottom)+72px),94px)] left-[22px] rounded-full p-[26px] text-lg ${
+          canUndo ? 'text-gray-500' : 'text-gray-300'
         }`}
+        ripple={true}
         onClick={handleUndoClick}
       >
         <ImUndo />
-      </button>
+      </Button>
     </>
   );
 }
