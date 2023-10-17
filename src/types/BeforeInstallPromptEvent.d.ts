38b8ae31c68f5f65a69ae11957d3f09b1eb0ae25
5656831 <hasegawatatsuya206@gmail.com>
2023-10-17 15:04:04 +0900

    Refactor: BeforeInstallPromptEvent Type 型定義ファイル 抽出

---
+++ src/types/BeforeInstallPromptEvent.d.ts
@@ -0,0 +1,9 @@
+export interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: Array<string>;
+  readonly userChoice: Promise<{
+    outcome: 'accepted' | 'dismissed';
+    platform: string;
+  }>;
+  deferredPrompt: BeforeInstallPromptEvent | null;
+  prompt(): Promise<void>;
+}
