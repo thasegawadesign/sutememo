7dabe21d92934af6880a698240120954de165c96
5656831 <hasegawatatsuya206@gmail.com>
2023-10-22 19:57:42 +0900

    Refactor: カラー選択ボタン, コンポーネントに切り出し

---
+++ src/app/components/theme-select-button.tsx
@@ -0,0 +1,49 @@
+import { useContext } from 'react';
+import { ThemeContext, ThemeType } from '../contexts/theme-provider';
+import CheckedIcon from './checked-icon';
+import { Radio } from '../contexts/material-providers';
+
+interface Props extends ThemeType {
+  id: string;
+  name: string;
+  defaultChecked?: boolean;
+}
+
+export default function ThemeSelectButton(props: Props) {
+  const {
+    id,
+    name,
+    baseColor,
+    mainColor,
+    mode,
+    defaultChecked = false,
+  } = props;
+  const { setTheme } = useContext(ThemeContext);
+
+  return (
+    <button
+      onClick={() =>
+        setTheme({
+          baseColor: baseColor,
+          mainColor: mainColor,
+          mode: mode,
+        })
+      }
+    >
+      <Radio
+        defaultChecked={defaultChecked}
+        name={name}
+        id={id}
+        icon={<CheckedIcon />}
+        className="checked:before:bg-blue-gray-50 hover:bg-blue-gray-50 hover:brightness-[102%] active:bg-blue-gray-50"
+        label={
+          <div className="grid rounded-full border border-gray-300">
+            <div className={`h-6 w-12 rounded-t-full bg-${baseColor}`} />
+            <div className={`h-6 w-12 rounded-b-full bg-${mainColor}`} />
+          </div>
+        }
+        ripple={false}
+      />
+    </button>
+  );
+}
