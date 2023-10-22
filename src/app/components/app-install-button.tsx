245a4d099f18b6b53f85ff6926920a5390ed6aa3
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-10-23 02:40:08 +0900

    Fix: インストールボタン, スタイル

--- src/app/components/app-install-button.tsx
+++ src/app/components/app-install-button.tsx
@@ -1,6 +1,8 @@
 import { useContext, useRef } from 'react';
 import { Button } from '../contexts/material-providers';
 import { ShowAppInstallButtonContext } from '../contexts/show-app-install-button-provider';
+import { ThemeContext } from '../contexts/theme-provider';
+import { bgVariants, colorVariants } from '../utils/colorVariants';
 
 type Props = {
   handleAppInstallButtonClick: () => void;
@@ -11,13 +13,16 @@ export default function AppInstallButton(props: Props) {
   const appInstallButtonRef = useRef<HTMLButtonElement>(null);
   const { showAppInstallButton } = useContext(ShowAppInstallButtonContext);
 
+  const theme = useContext(ThemeContext);
+  const { baseColor, mainColor } = theme;
+
   return (
     <>
       {showAppInstallButton && (
         <Button
           ref={appInstallButtonRef}
           onClick={handleAppInstallButtonClick}
-          className="flex items-center gap-0.5 rounded-full bg-main px-6 py-1.5 font-semibold text-white !shadow-none filter transition hover:brightness-95"
+          className={`flex items-center gap-0.5 rounded-full px-6 py-1.5 font-semibold !shadow-none transition hover:brightness-95 ${bgVariants[mainColor]} ${colorVariants[baseColor]}`}
         >
           <span className="select-none text-sm">入手</span>
         </Button>
