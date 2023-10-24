e675f94c6c29116794bc141d38ecba647b57541c
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-10-24 23:29:13 +0900

    Chore: google analytics, inlineHTML minify

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,9 +1,106 @@
 import Script from 'next/script';
 import { env } from 'process';
+import { minify } from 'terser';
 
-export default function Analytics() {
+export default async function Analytics() {
   const cookieDomain = env.COOKIE_DOMAIN;
   const ANALYTICS_ID = env.NEXT_PUBLIC_ANALYTICS_ID;
+
+  const customGaScript = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('set', {
+    cookie_domain: '${cookieDomain}',
+    cookie_flags: 'SameSite=None;Secure',
+  });
+  gtag('config', '${ANALYTICS_ID}');
+  document.addEventListener(
+    'blur',
+    (event) => {
+      if (!event.target) return;
+      const isTextbox = event.target.role === 'textbox';
+      if (isTextbox) {
+        gtag('event', 'edit_todo');
+      }
+    },
+    true,
+  );
+  document.querySelector('[aria-label="Add"]')?.addEventListener('click', (event) => {
+    gtag('event', 'add_todo', {
+      event_category: 'Add',
+      event_label: 'Click',
+    });
+  });
+  document.querySelector('[aria-label="Undo"]')?.addEventListener('click', (event) => {
+    gtag('event', 'undo', {
+      event_category: 'Undo',
+      event_label: 'Click',
+    });
+  });
+  document.querySelector('[aria-label="Redo"]')?.addEventListener('click', (event) => {
+    gtag('event', 'redo', {
+      event_category: 'Redo',
+      event_label: 'Click',
+    });
+  });
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) return;
+    if (event.key === 'Enter') {
+      const target = event.target;
+      if (target?.nodeName !== 'BODY') return;
+      gtag('event', 'add_todo', {
+        event_category: 'Add',
+        event_label: 'Keydown',
+      });
+    }
+  });
+  document.addEventListener('click', (event) => {
+    if (!event.target) return;
+    const isSVG = event.target.localName === 'svg';
+    const isButton = event.target.localName === 'button';
+    const target =
+      (isSVG && event.target.parentElement) || (isButton && event.target);
+    const isDeleteButton = target.ariaLabel === 'Delete';
+    if (isDeleteButton) gtag('event', 'delete_todo');
+  });
+  document.addEventListener('mouseup', (event) => {
+    if (!event.target) return;
+    const isSVG = event.target.localName === 'svg';
+    const isButton = event.target.localName === 'button';
+    const target =
+      (isSVG && event.target.parentElement) || (isButton && event.target);
+    const isDraggableButton = target.ariaRoleDescription === 'sortable';
+    if (isDraggableButton)
+      gtag('event', 'sort_todo', {
+        event_category: 'Sort',
+        event_label: 'MouseUp',
+      });
+  });
+  document.addEventListener('touchend', (event) => {
+    if (!event.target) return;
+    const isSVG = event.target.localName === 'svg';
+    const isButton = event.target.localName === 'button';
+    const target =
+      (isSVG && event.target.parentElement) || (isButton && event.target);
+    const isDraggableButton = target.ariaRoleDescription === 'sortable';
+    if (isDraggableButton)
+      gtag('event', 'sort_todo', {
+        event_category: 'Sort',
+        event_label: 'TouchEnd',
+      });
+  });
+  window.addEventListener('appinstalled', (event) => {
+    gtag('event', 'install_pwa', {
+      event_category: 'PWA',
+      event_label: 'Install',
+    });
+  });
+`;
+
+  const result = await minify(customGaScript);
+  const minifiedCode = result.code as string;
+
   if (env.NODE_ENV !== 'production') {
     return <></>;
   }
@@ -18,103 +115,7 @@ export default function Analytics() {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}`}
       ></Script>
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('set', {
-              cookie_domain: '${cookieDomain}',
-              cookie_flags: 'SameSite=None;Secure',
-            });
-            gtag('config', '${ANALYTICS_ID}');
-            document.addEventListener(
-              'blur',
-              (event) => {
-                if (!event.target) return;
-                const isTextbox = event.target.role === 'textbox';
-                if (isTextbox) {
-                  gtag('event', 'edit_todo');
-                }
-              },
-              true,
-            );
-            document.querySelector('[aria-label="Add"]')?.addEventListener('click', (event) => {
-              gtag('event', 'add_todo', {
-                event_category: 'Add',
-                event_label: 'Click',
-              });
-            });
-            document.querySelector('[aria-label="Undo"]')?.addEventListener('click', (event) => {
-              gtag('event', 'undo', {
-                event_category: 'Undo',
-                event_label: 'Click',
-              });
-            });
-            document.querySelector('[aria-label="Redo"]')?.addEventListener('click', (event) => {
-              gtag('event', 'redo', {
-                event_category: 'Redo',
-                event_label: 'Click',
-              });
-            });
-            window.addEventListener('keydown', (event) => {
-              if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) return;
-              if (event.key === 'Enter') {
-                const target = event.target;
-                if (target?.nodeName !== 'BODY') return;
-                gtag('event', 'add_todo', {
-                  event_category: 'Add',
-                  event_label: 'Keydown',
-                });
-              }
-            });
-            document.addEventListener('click', (event) => {
-              if (!event.target) return;
-              const isSVG = event.target.localName === 'svg';
-              const isButton = event.target.localName === 'button';
-              const target =
-                (isSVG && event.target.parentElement) || (isButton && event.target);
-              const isDeleteButton = target.ariaLabel === 'Delete';
-              if (isDeleteButton) gtag('event', 'delete_todo');
-            });
-            document.addEventListener('mouseup', (event) => {
-              if (!event.target) return;
-              const isSVG = event.target.localName === 'svg';
-              const isButton = event.target.localName === 'button';
-              const target =
-                (isSVG && event.target.parentElement) || (isButton && event.target);
-              const isDraggableButton = target.ariaRoleDescription === 'sortable';
-              if (isDraggableButton)
-                gtag('event', 'sort_todo', {
-                  event_category: 'Sort',
-                  event_label: 'MouseUp',
-                });
-            });
-            document.addEventListener('touchend', (event) => {
-              if (!event.target) return;
-              const isSVG = event.target.localName === 'svg';
-              const isButton = event.target.localName === 'button';
-              const target =
-                (isSVG && event.target.parentElement) || (isButton && event.target);
-              const isDraggableButton = target.ariaRoleDescription === 'sortable';
-              if (isDraggableButton)
-                gtag('event', 'sort_todo', {
-                  event_category: 'Sort',
-                  event_label: 'TouchEnd',
-                });
-            });
-            window.addEventListener('appinstalled', (event) => {
-              gtag('event', 'install_pwa', {
-                event_category: 'PWA',
-                event_label: 'Install',
-              });
-            });
-          `,
-        }}
-      />
+      <Script id="ga">{minifiedCode}</Script>
     </>
   );
 }
