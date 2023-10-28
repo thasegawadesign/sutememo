724c48794c66e82215547303d8c967a5e25cc717
5656831 <hasegawatatsuya206@gmail.com>
2023-10-28 23:07:51 +0900

    Remove: コメント削除

--- src/utils/colorVariants.ts
+++ src/utils/colorVariants.ts
@@ -18,15 +18,6 @@ export const bgVariants: {
   ...generateRadixMappingObj('bg-'),
 };
 
-// export const bgHoverVariants: {
-//   [key: string]: string;
-// } = {
-//   white: 'hover-bg:white',
-//   black: 'hover-bg:black',
-//   ...generateCustomMappingObj('hover:bg-'),
-//   ...generateRadixMappingObj('hover:bg-'),
-// };
-
 export const colorVariants: {
   [key: string]: string;
 } = {
