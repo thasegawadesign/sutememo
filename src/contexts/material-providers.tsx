5502a7ae405825c38340738920d40a0d074e93a4
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-10-28 17:56:01 +0900

    Fix: Drawerのoverlay, h-screenいっぱいに修正

--- src/contexts/material-providers.tsx
+++ src/contexts/material-providers.tsx
@@ -4,6 +4,7 @@ import {
   ThemeProvider,
   Button,
   Drawer,
+  DrawerStylesType,
   Accordion,
   AccordionHeader,
   AccordionBody,
@@ -19,3 +20,4 @@ export {
   AccordionBody,
   Radio,
 };
+export type { DrawerStylesType };
