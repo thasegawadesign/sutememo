6dd78bcb8a378e236f8b95fbb4e478edc85b9468
5656831 <hasegawatatsuya206@gmail.com>
2023-10-28 16:05:30 +0900

    Refactor: pageをserver componentに修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
-'use client';
-
-import { useContext } from 'react';
+import Screen from '@/components/screen';
 import Header from '@/components/header';
 import Main from '@/components/main';
-import { ThemeContext } from '@/contexts/theme-provider';
-import { bgVariants } from '@/utils/colorVariants';
 
 export default function Home() {
-  const theme = useContext(ThemeContext);
-  const { baseColor } = theme;
-
   return (
     <>
-      <div
-        className={`min-h-[100svh] overscroll-none pt-[env(safe-area-inset-top)] pwa:min-h-screen ${bgVariants[baseColor]}`}
-      >
+      <Screen>
         <Header />
         <Main />
-      </div>
+      </Screen>
     </>
   );
 }
