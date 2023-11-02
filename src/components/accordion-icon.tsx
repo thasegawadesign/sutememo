6984426fb7cd9a2f9067b5c30f7cde58a73680a8
5656831 <hasegawatatsuya206@gmail.com>
2023-11-02 10:36:14 +0900

    Fix: アコーディオンの矢印の向き 修正

--- src/components/accordion-icon.tsx
+++ src/components/accordion-icon.tsx
@@ -14,8 +14,8 @@ export default function AccorionIcon({
       strokeWidth={2.5}
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
-      className={clsx('h-5 w-5 transition-transform', {
-        '-rotate-180': id === open,
+      className={clsx('h-5 w-5 -rotate-90 transition-transform', {
+        '-rotate-0': id === open,
       })}
     >
       <path
