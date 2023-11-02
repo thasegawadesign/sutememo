661653103b84634062d7f3d7f474db19c0e45a1d
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-11-02 23:27:21 +0900

    Feat: Color Scheme, Theme 設定

---
+++ src/utils/updateBodyBackgroundColor.ts
@@ -0,0 +1,14 @@
+import { SafeColorList } from '@/types/ColorList';
+import { bgVariants } from '@/utils/colorVariants';
+
+export const updateBodyBackgroundColor = (backgroundColor: SafeColorList) => {
+  const BODY = document.body;
+  const bodyClassList = BODY.classList;
+  let matchedClassName;
+  const regEx = new RegExp(/^bg-/);
+  bodyClassList.forEach((className) => {
+    matchedClassName = className.match(regEx)?.input;
+    BODY.classList.remove(String(matchedClassName));
+  });
+  BODY.classList.add(`${bgVariants[backgroundColor]}`);
+};
