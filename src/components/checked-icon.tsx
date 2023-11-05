7a0471d3d00bc5309e8ed31f9b3c399a85457466
5656831 <hasegawatatsuya206@gmail.com>
2023-11-05 22:48:11 +0900

    Style: アコーディオン ヘッダー, スタイル修正

--- src/components/checked-icon.tsx
+++ src/components/checked-icon.tsx
@@ -13,7 +13,7 @@ export default function CheckedIcon() {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       className={clsx('h-full w-full scale-105', {
-        'text-gray-900': mode === 'light',
+        'text-customGray-9': mode === 'light',
         'text-white-12': mode === 'dark',
       })}
     >
