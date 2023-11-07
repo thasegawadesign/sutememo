0151d3b87ce1b4b6bb9412dd86d67bf4a34233ab
5656831 <hasegawatatsuya206@gmail.com>
2023-11-08 02:06:28 +0900

    Remove: 必須ではないサイズのアイコン 削除

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -37,18 +37,6 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: 'maskable',
       },
       {
-        src: '/icons/icon-256x256.png',
-        sizes: '256x256',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
-        src: '/icons/icon-384x384.png',
-        sizes: '384x384',
-        type: 'image/png',
-        purpose: 'any',
-      },
-      {
         src: '/icons/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
