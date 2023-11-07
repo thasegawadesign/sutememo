7a84e9cffdf19a1e5230b3e44029a6cf8650d4fd
5656831 <hasegawatatsuya206@gmail.com>
2023-11-08 01:07:39 +0900

    Fix: ローディング画面 常に単色のGray

--- src/components/fullscreen-solid-backgound.tsx
+++ src/components/fullscreen-solid-backgound.tsx
@@ -1,17 +1,16 @@
-import clsx from 'clsx';
-import { useContext } from 'react';
-
-import { ThemeContext } from '@/contexts/theme-provider';
+import { SafeColorList } from '@/types/ColorList';
 import { bgVariants } from '@/utils/colorVariants';
 
-export default function FullScreenSolidBackgound() {
-  const { baseColor } = useContext(ThemeContext);
+type Props = {
+  colorName: SafeColorList;
+};
+
+export default function FullScreenSolidBackgound(props: Props) {
+  const { colorName } = props;
 
   return (
     <div
-      className={clsx(
-        `min-h-[100svh] w-full pwa:min-h-screen ${bgVariants[baseColor]}`,
-      )}
+      className={`min-h-[100svh] w-full pwa:min-h-screen ${bgVariants[colorName]}`}
     />
   );
 }
