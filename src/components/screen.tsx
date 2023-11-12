0c21c2b6145fb73e9c9dd5e542e6798dc1aa16b4
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-11-12 11:36:40 +0900

    Fix: システムの設定(ライト/ダーク) 反映するよう修正

--- src/components/screen.tsx
+++ src/components/screen.tsx
@@ -11,16 +11,54 @@ import FullScreenSolidBackgound from '@/components/fullscreen-solid-backgound';
 import { IsDarkModeSelectContext } from '@/contexts/is-dark-mode-select-provider';
 import { IsSystemModeSelectContext } from '@/contexts/is-system-mode-select-provider';
 import { ThemeContext } from '@/contexts/theme-provider';
+import useMediaPrefersColorScheme from '@/hooks/useMediaPrefersColorScheme';
 import { bgVariants } from '@/utils/colorVariants';
+import { handlePrefersColorSchemeChange } from '@/utils/handlePrefersColorSchemeChange';
 import { updateBodyBackgroundColor } from '@/utils/updateBodyBackgroundColor';
 import { updateHtmlColorScheme } from '@/utils/updateHtmlColorScheme';
 import { updateMetaThemeColor } from '@/utils/updateMetaThemeColor';
 
 export default function Screen({ children }: { children: React.ReactNode }) {
   const { isSystemModeSelect } = useContext(IsSystemModeSelectContext);
-  const { isDarkModeSelect } = useContext(IsDarkModeSelectContext);
-  const { baseColor, mainColor, mode } = useContext(ThemeContext);
+  const { isDarkModeSelect, setIsDarkModeSelect } = useContext(
+    IsDarkModeSelectContext,
+  );
+  const { baseColor, mainColor, mode, setTheme } = useContext(ThemeContext);
   const [isLoading, setIsLoading] = useState(true);
+  const prefersColorScheme = useMediaPrefersColorScheme();
+
+  useEffect(() => {
+    if (!globalThis.window) return;
+    if (isSystemModeSelect) {
+      setTheme({
+        baseColor,
+        mainColor,
+        mode: prefersColorScheme,
+      });
+      setIsDarkModeSelect(prefersColorScheme === 'dark');
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener(
+          'change',
+          handlePrefersColorSchemeChange.bind(isSystemModeSelect),
+        );
+    }
+
+    return () =>
+      window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .removeEventListener(
+          'change',
+          handlePrefersColorSchemeChange.bind(isSystemModeSelect),
+        );
+  }, [
+    baseColor,
+    isSystemModeSelect,
+    mainColor,
+    prefersColorScheme,
+    setIsDarkModeSelect,
+    setTheme,
+  ]);
 
   useEffect(() => {
     updateHtmlColorScheme(mode);
