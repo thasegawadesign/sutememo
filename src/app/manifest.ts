d06543eaaa99e36b100fbf8cafd3fcbbe3da652f
5656831 <hasegawatatsuya206@gmail.com>
2023-11-14 05:42:57 +0900

    Add: maskable icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -35,10 +35,22 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: 'any',
       },
       {
+        src: '/icons/maskable-app-icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
         src: '/icons/rounded-app-icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
+      },
+      {
+        src: '/icons/maskable-app-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/icons/rounded-app-icon-1024x1024.png',
