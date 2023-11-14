2b5b56e99b2ed31ebce6dad93d422c5e856c6a05
5656831 <hasegawatatsuya206@gmail.com>
2023-11-14 13:01:26 +0900

    Fix: root, settingsページ間 遷移での画面のチラつきを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import ContentsWrapper from '@/components/contents-wrapper';
-import Screen from '@/components/screen';
 
 export default function Home() {
   return (
     <>
-      <Screen>
-        <ContentsWrapper />
-      </Screen>
+      <ContentsWrapper />
     </>
   );
 }
