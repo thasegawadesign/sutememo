bef247e1e499c64a800853e7ab86e3847024ca80
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2023-12-20 00:07:23 +0900

    Fix: appInstallButtonの表示制御

--- src/app/components/AppInstallButton.tsx
+++ src/app/components/AppInstallButton.tsx
@@ -1,16 +1,18 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, RefObject } from 'react';
 
 type Props = {
   handleAppInstallButtonClick: (event: MouseEvent) => void;
+  appInstallButtonRef: RefObject<HTMLButtonElement>;
 };
 
 export default function AppInstallButton(props: Props) {
-  const { handleAppInstallButtonClick } = props;
+  const { handleAppInstallButtonClick, appInstallButtonRef } = props;
   return (
     <>
       <button
+        ref={appInstallButtonRef}
         onClick={handleAppInstallButtonClick}
-        className="flex items-center gap-0.5 rounded-full bg-main px-6 py-1.5 font-semibold text-white"
+        className="flex items-center gap-0.5 rounded-full bg-main px-6 py-1.5 font-semibold text-white filter transition hover:brightness-95"
       >
         <span className="text-sm">入手</span>
       </button>
