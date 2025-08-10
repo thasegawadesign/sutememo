5aaecef3c680321f1d59e56a6bbf22514268cc9a
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-08-10 21:36:56 +0900

    fix: google analytics

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,106 +1,7 @@
-import { minify } from '@swc/core';
-import Script from 'next/script';
-
-import { ariaLabel } from '@/utils/ariaLabel';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default async function Analytics() {
-  const COOKIE_DOMAIN = process.env.NEXT_PUBLIC_COOKIE_DOMAIN;
   const ANALYTICS_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
-
-  const customGaScript = `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('set', {
-    cookie_domain: '${COOKIE_DOMAIN}',
-    cookie_flags: 'SameSite=None;Secure',
-  });
-  gtag('config', '${ANALYTICS_ID}');
-  document.addEventListener(
-    'blur',
-    (event) => {
-      if (!event.target) return;
-      const isTextbox = event.target.role === 'textbox';
-      if (isTextbox) {
-        gtag('event', 'edit_todo');
-      }
-    },
-    true,
-  );
-  document.querySelector('[aria-label="${ariaLabel.addButton}"]')?.addEventListener('click', (event) => {
-    gtag('event', 'add_todo', {
-      event_category: 'Add',
-      event_label: 'Click',
-    });
-  });
-  document.querySelector('[aria-label="${ariaLabel.undoButton}"]')?.addEventListener('click', (event) => {
-    gtag('event', 'undo', {
-      event_category: 'Undo',
-      event_label: 'Click',
-    });
-  });
-  document.querySelector('[aria-label="${ariaLabel.redoButton}"]')?.addEventListener('click', (event) => {
-    gtag('event', 'redo', {
-      event_category: 'Redo',
-      event_label: 'Click',
-    });
-  });
-  window.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) return;
-    if (event.key === 'Enter') {
-      const target = event.target;
-      if (target?.nodeName !== 'BODY') return;
-      gtag('event', 'add_todo', {
-        event_category: 'Add',
-        event_label: 'Keydown',
-      });
-    }
-  });
-  document.addEventListener('click', (event) => {
-    if (!event.target) return;
-    const isSVG = event.target.localName === 'svg';
-    const isButton = event.target.localName === 'button';
-    const target =
-      (isSVG && event.target.parentElement) || (isButton && event.target);
-    const isDeleteButton = target.ariaLabel === '${ariaLabel.deleteButton}';
-    if (isDeleteButton) gtag('event', 'delete_todo');
-  });
-  document.addEventListener('mouseup', (event) => {
-    if (!event.target) return;
-    const isSVG = event.target.localName === 'svg';
-    const isButton = event.target.localName === 'button';
-    const target =
-      (isSVG && event.target.parentElement) || (isButton && event.target);
-    const isDraggableButton = target.ariaRoleDescription === 'sortable';
-    if (isDraggableButton)
-      gtag('event', 'sort_todo', {
-        event_category: 'Sort',
-        event_label: 'MouseUp',
-      });
-  });
-  document.addEventListener('touchend', (event) => {
-    if (!event.target) return;
-    const isSVG = event.target.localName === 'svg';
-    const isButton = event.target.localName === 'button';
-    const target =
-      (isSVG && event.target.parentElement) || (isButton && event.target);
-    const isDraggableButton = target.ariaRoleDescription === 'sortable';
-    if (isDraggableButton)
-      gtag('event', 'sort_todo', {
-        event_category: 'Sort',
-        event_label: 'TouchEnd',
-      });
-  });
-  window.addEventListener('appinstalled', (event) => {
-    gtag('event', 'install_pwa', {
-      event_category: 'PWA',
-      event_label: 'Install',
-    });
-  });
-`;
-
-  const result = await minify(customGaScript);
-  const minifiedCode = result.code as string;
 
   if (process.env.NODE_ENV !== 'production') {
     return <></>;
@@ -112,11 +13,7 @@ export default async function Analytics() {
 
   return (
     <>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}`}
-      ></Script>
-      <Script id="gtag">{minifiedCode}</Script>
+      <GoogleAnalytics gaId={ANALYTICS_ID || ''} />
     </>
   );
 }
